46e6c81b76145df993eeb407abe0a248abbfa877
Aldy Satria Gumilar <pilusnarziz3@gmail.com>
2017-03-20 00:42:20 +0700

    tambahan model

---
+++ application/modules/lv_webmin/models/model_fakultas.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Model_fakultas extends CI_Model {
+
+  public $table = 'dospres_fakultas';
+  public $id = 'id_fakultas';
+
+  public function data()
+  {
+    $query = $this->db->get('dospres_fakultas');
+    return $query->result_array();
+  }
+}
