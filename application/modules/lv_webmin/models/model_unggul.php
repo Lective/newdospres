6458c85ddb3b8ddbb8db64303baab5a88a4a8f7e
Aldy Satria Gumilar <pilusnarziz3@gmail.com>
2017-03-29 17:55:35 +0700

    update model

---
+++ application/modules/lv_webmin/models/model_unggul.php
@@ -0,0 +1,46 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Model_unggul extends CI_Model {
+
+  public $table = 'dospres_prestasi_unggul';
+  public $id = 'id_prestasi_unggul';
+
+  public function data()
+  {
+    $query = $this->db->get('dospres_prestasi_unggul');
+    return $query->result_array();
+  }
+
+  public function findData($id)
+  {
+    $hasil = $this->db->where('id_prestasi_unggul', $id)
+                       ->limit(1) 
+                       ->get('dospres_prestasi_unggul');
+    if ($hasil->num_rows() > 0) {
+      return $hasil->row();
+    } else {
+      return array();
+    }
+  }
+
+  public function addData($data)
+  {
+    $this->db->insert('dospres_prestasi_unggul', $data);
+    return TRUE; 
+  }
+
+  public function deleteData($id)
+  {
+    $this->db->where('id_prestasi_unggul', $id)
+             ->delete('dospres_prestasi_unggul');
+  }
+
+  public function updateData($id, $data_kaprodi)
+  {
+    $this->db->where('id_prestasi_unggul', $id)
+             ->update('dospres_prestasi_unggul', $data_kaprodi);
+  }
+  
+
+}
