a26165f99de07c9fac7f55c1658b1f8b1b4fe553
andialrizki <muhammad.andialrizki@gmail.com>
2017-04-05 13:59:07 +0700

    add dashboard dppm

---
+++ application/modules/themes/views/dppm/sidebar.php
@@ -0,0 +1,63 @@
+<div class="site-menubar">
+  <div class="site-menubar-header">
+      <div class="cover overlay">
+        <img class="cover-image" src="<?php echo base_url('public/img/components/bg-user.jpg')?>" alt="...">
+        <div class="overlay-panel vertical-align">
+          <div class="vertical-align-middle">
+            <span class="avatar avatar-lg" >
+              <img src="<?php echo base_url('public/img/admin.png') ?>" alt="" >
+            </span>
+            <span><p class="site-menubar-user">DPPM <br>DPPM - UMM</p></span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="site-menubar-body">
+      <div>
+        <div>
+          <ul class="site-menu">
+            <li class="site-menu-category">MENU UMUM</li>
+            <li class="site-menu-item">
+              <a class="animsition-link" href="<?php echo base_url('webmin') ?>">
+                <i class="site-menu-icon wb-home" aria-hidden="true"></i>
+                <span class="site-menu-title">Dashboard</span>
+              </a>
+            </li>
+            <li class="site-menu-item">
+              <a class="animsition-link  tooltip-primary tooltip-scale" href="<?php echo base_url('webmin/laporan') ?>" data-toggle="tooltip" data-placement="top" data-original-title="Laporan Calon Dosen Berprestasi">
+                <i class="site-menu-icon wb-copy" aria-hidden="true"></i>
+                <span class="site-menu-title">Laporan</span>
+              </a>
+            </li>
+            <li class="site-menu-category">MENU KELOLA</li>
+            <li class="site-menu-item">
+              <a class="animsition-link  tooltip-primary tooltip-scale" href="<?php echo base_url('webmin/prestasi-unggul') ?>" data-toggle="tooltip" data-placement="top" data-original-title="Kelola Formulir Prestasi Unggul">
+                <i class="site-menu-icon wb-star-half" aria-hidden="true"></i>
+                <span class="site-menu-title">Formulir Prestasi Unggul</span>
+              </a>
+            </li>
+            <li class="site-menu-item">
+              <a class="animsition-link tooltip-primary tooltip-scale" href="<?php echo base_url('webmin/kaprodi-berprestasi') ?>" data-toggle="tooltip" data-placement="top" data-original-title="Kelola Kaprodi Berprestasi">
+                <i class="site-menu-icon fa fa-users" aria-hidden="true"></i>
+                <span class="site-menu-title">Kaprodi Berprestasi</span>
+              </a>
+            </li>
+            <li class="site-menu-item tooltip-primary tooltip-scale">
+              <a class="animsition-link tooltip-primary tooltip-scale" href="<?php echo base_url('webmin/dosen-berprestasi') ?>" data-toggle="tooltip" data-placement="top" data-original-title="Kelola Dosen Berprestasi">
+                <i class="site-menu-icon fa fa-users" aria-hidden="true"></i>
+                <span class="site-menu-title">Dosen Berprestasi</span>
+              </a>
+            </li>
+            <li class="site-menu-item">
+              <a class="animsition-link tooltip-primary tooltip-scale" href="<?php echo base_url('webmin/managemen-akun') ?>" data-toggle="tooltip" data-placement="top" data-original-title="Kelola Managemen Akun">
+                <i class="site-menu-icon fa fa-gears" aria-hidden="true"></i>
+                <span class="site-menu-title">Managemen Akun</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+</div>
+
+  
