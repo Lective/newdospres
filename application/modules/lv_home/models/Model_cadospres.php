b52c86708801a906c5969870f4d1ad94d2ba7f8f
Aldy Satria Gumilar <pilusnarziz3@gmail.com>
2017-04-21 16:19:45 +0700

    update voting -> lanjut fikri

--- application/modules/lv_home/models/Model_cadospres.php
+++ application/modules/lv_home/models/Model_cadospres.php
@@ -4,12 +4,22 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Model_cadospres extends CI_Model {
 
   public $table = 'dospres_view_voting_dospres';
-  public $id = 'id_calon_dospres';
+  public $nidn = 'nidn';
 
   public function data()
   {
     $query = $this->db->get('dospres_view_voting_dospres');
     return $query->result_array();
+  }
+  public function get($nidn){
+  	$hasil = $this->db->where('nidn', $nidn)
+                       ->limit(1) 
+                       ->get('dospres_view_voting_dospres');
+    if ($hasil->num_rows() > 0) {
+      return $hasil->row();
+    } else {
+      return array();
+    }
   }
 
 }
