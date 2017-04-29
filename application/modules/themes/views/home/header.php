56211dbcbf2c6e4f49a4f7d44c2ed721899eab32
andialrizki <muhammad.andialrizki@gmail.com>
2017-04-29 08:38:58 +0700

    edit home header

--- application/modules/themes/views/home/header.php
+++ application/modules/themes/views/home/header.php
@@ -1,14 +1,12 @@
 <!DOCTYPE html>
-<!-- saved from url=(0055)https://guest15194rhw-zjo1.bitblox.me/?ts=1492315529302 -->
 <html lang="en">
 
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <title>Dosen Berprestasi - Universitas Muhammadiyah Malang</title>
-    <meta name="description" content="">
+    <meta name="description" content="Sistem Informasi Dosen Berprestasi - UMM">
     <meta name="viewport" content="width=device-width,initial-scale=1">
     <meta name="author" content="andialrizki">
-    <link rel="stylesheet" href="">
     <link rel="stylesheet" href="<?php echo base_url()?>public/global/fonts/font-awesome/font-awesome.css" media="screen">
     <link rel="stylesheet" type="text/css" href="<?php echo base_url()?>public\base\assets\examples\css\pages\profile.css"> 
     <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>public/global/css/bootstrap.min.css">
